4a9856512175a367135f157e5a2b0057ee56c5af
amshinski <amshinski@gmail.com>
2025-04-11 20:07:27 +0300

    Remove transition from textarea and input

--- resources/js/Components/TextArea.vue
+++ resources/js/Components/TextArea.vue
@@ -12,7 +12,7 @@ defineEmits(['update:modelValue']);
         :value="modelValue"
         :class="{ '!border-red-400/60': errors }"
         class="w-full px-4 py-3 bg-white/5 border border-white/10 rounded-xl focus:ring-2 focus:ring-indigo-400/50
-            focus:border-transparent text-gray-200 placeholder-gray-400/60 h-32 transition-all backdrop-blur-sm"
+            focus:border-transparent text-gray-200 placeholder-gray-400/60 h-32 backdrop-blur-sm"
         rows="3"
         @input="$emit('update:modelValue', $event.target.value)">
     </textarea>
--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -24,7 +24,7 @@ defineExpose({ focus: () => input.value.focus() });
         ref="input"
         :class="{ '!border-red-400/60': errors }"
         class="w-full px-4 py-3 bg-white/5 border border-white/10 rounded-xl focus:ring-2 focus:ring-indigo-400/50
-            focus:border-transparent text-gray-200 placeholder-gray-400/60 transition-all backdrop-blur-sm"
+            focus:border-transparent text-gray-200 placeholder-gray-400/60 backdrop-blur-sm"
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
     >
